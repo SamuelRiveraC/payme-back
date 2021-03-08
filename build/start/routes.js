493c19d2dca1f3cef808a4ef9b8d14cf1ea49e1d
samuelriverac <samuelrivera64@gmail.com>
2021-03-08 01:13:47 -0400

    fixed

--- build/start/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./routes/users.ts");
-require("./routes/transactions.ts");
-require("./routes/bank_accounts.ts");
-require("./routes/notifications.ts");
-//# sourceMappingURL=routes.js.map
